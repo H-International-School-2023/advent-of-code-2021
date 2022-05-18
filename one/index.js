c5153a529c25f951830d41124c43cf45cad5cf3b
Andrea Beggiato <andrea.beggiato@geekcups.com>
2022-05-18 12:45:20 +0200

    day1 solved

--- one/index.js
+++ one/index.js
@@ -3,3 +3,12 @@ const fs = require('fs');
 const dataString = fs.readFileSync('./data', 'utf-8');
 
 const data = dataString.split('\n').map((row) => parseInt(row));
+
+let count = 0;
+for (let i = 1; i < data.length; i = i + 1) {
+  if (data[i] > data[i - 1]) {
+    count = count + 1;
+  }
+}
+
+console.log(count);
